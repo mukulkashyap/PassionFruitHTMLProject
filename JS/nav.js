f35738411f25644d32d0b155f2bc1526179f4375
Mukul Kashyap <mukulkashyap@gmail.com>
2023-12-31 20:31:31 +0530

    Modify Common Header

---
+++ JS/nav.js
@@ -0,0 +1,4 @@
+
+let value="<header class='sticky-header'><h1>PASSION FRUIT</h1><nav><ul><li> <a href='home.html' style='color:orange;'> Home </a> </li><li> <a href='about.html' style='color:orange;'> About </a> </li><li> <a href='council.html' style='color:orange;'> Student Council </a> </li> <li> <a href='archives.html' style='color:orange;'> Archives </a> </li><li> <a href='calender.html' style='color:orange;'> Calendar </a> </li><li> <a href='scores.html' style='color:orange;'> Scores </a> </li></ul></nav></header>"
+document.getElementById("navheader").innerHTML=value;
+
